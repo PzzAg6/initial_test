7bc8ebdd933eb0efc72525d659b0e685a1706270
PzzAg6 <57948829+PzzAg6@users.noreply.github.com>
2020-09-08 13:01:02 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    cout << "want to input some data\n";
+
+    cout << "1,2,3\n";
+
+    cout << "4,5,6\n";
+
+
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
